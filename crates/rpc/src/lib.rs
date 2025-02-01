f75df1159eb814329878f969bc0470f9e43f6bc8
Lucas Vienna <dev@lucasvienna.dev>
2025-02-01 13:50:16 +0100

    chore: suppress warning in rpc module
    It's unfinished and doesn't really need to be
    mucking up the CI for now

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -3,6 +3,12 @@
 #[macro_use]
 mod macros;
 
+#[allow(unused_variables)]
+#[allow(dead_code)]
 pub mod buffer;
+#[allow(unused_variables)]
+#[allow(dead_code)]
 pub mod net;
+#[allow(unused_variables)]
+#[allow(dead_code)]
 pub mod packet;
